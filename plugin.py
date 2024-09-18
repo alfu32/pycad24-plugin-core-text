7bec5e6cbf20c2259b7507f12ff3df19f3d06634
Ioan Alferaru <ioan.alferaru@gmail.com>
2024-09-18 18:28:53 +0200

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -8,7 +8,10 @@ from pycad.ComponentLayers import LayerModel
 from pycad.Drawable import Drawable
 from pycad.Plugin import PluginInterface
 
-class PycadCoreLinePlugin(PluginInterface):
+def init_plugin(app):
+    print("initializing text tool plugin",flush=True)
+  
+class PycadCoreTextPlugin(PluginInterface):
   
     class Line(Drawable):
         changed = Signal(object)  # Define a custom signal with a generic object type
